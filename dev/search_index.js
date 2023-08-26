181c9d3213955dd491b49aaedf56fe10fd82a9be
Documenter.jl <documenter@juliadocs.github.io>
2023-08-26 03:25:05 +0000

    build based on ff84cd5

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SequentialCompression","category":"page"},{"location":"#SequentialCompression","page":"Home","title":"SequentialCompression","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SequentialCompression.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SequentialCompression]","category":"page"}]
+}
