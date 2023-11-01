d2b63507ba6dedebb5866189b86a4d37f95a2cc2
Átila Saraiva <atilasaraiva@gmail.com>
2023-11-01 14:43:39 -0600

    adding precision as a paramenter for the benchmark

--- paper/experiments/3_benchmark.jl
+++ paper/experiments/3_benchmark.jl
@@ -3,7 +3,7 @@ using ZfpCompression
 using CairoMakie
 using DataFrames
 
-function compressionthroughput(; rate::Int=0, tol::Real=0, precision::Real=0, inmemory::Bool=true)
+function compressionthroughput(; rate::Int=0, tol::Real=0, precision::Int=0, inmemory::Bool=true)
     dummyData = rand(1000,1000,10)
     cp = SeqCompressor(Float64, 1000, 1000, rate=rate, tol=tol, precision=precision, inmemory=inmemory)
 
@@ -53,7 +53,7 @@ function decompressionthroughput2(cp)
     return throughput
 end
 
-function compressionthroughput3(; rate::Int=0, tol::Real=0, precision::Real=0, inmemory::Bool=true)
+function compressionthroughput3(; rate::Int=0, tol::Real=0, precision::Int=0, inmemory::Bool=true)
     dummyData = rand(1000,1000,10)
     cp = Vector{Vector{UInt8}}(undef, 10)
     wtime0 = Base.time()
@@ -78,7 +78,7 @@ function decompressionthroughput3(cp)
     return throughput
 end
 
-function throughputs(; rate::Int=0, tol::Real=0, precision::Real=0, inmemory::Bool=true)
+function throughputs(; rate::Int=0, tol::Real=0, precision::Int=0, inmemory::Bool=true)
     comp_throughput, cp = compressionthroughput(rate=rate, tol=tol, precision=precision, inmemory=inmemory)
     decomp_throughput = decompressionthroughput(cp)
     return comp_throughput, decomp_throughput
@@ -107,21 +107,21 @@ function rateTest()
     return df
 end
 
-function tolTest()
-    tol = [ 10.0^(-i) for i=1:10 ]
+function precisionTest()
+    precisions = 1:1:64
     df = DataFrame()
-    df.tol = tol
+    df.precision = precisions |> collect
 
-    values = zeros(length(df.rate),2)
-    for (i,rate) in enumerate(rates)
-        values[i,:] .= throughputs(rate=rate)
+    values = zeros(length(df.precision),2)
+    for (i,precision) in enumerate(precisions)
+        values[i,:] .= throughputs(precision=precision, inmemory=true)
     end
 
     df.compression_inmemory = values[:, 1]
     df.decompression_inmemory = values[:, 2]
 
-    for (i,rate) in enumerate(rates)
-        values[i,:] .= throughputs(rate=rate, inmemory=false)
+    for (i,precision) in enumerate(precisions)
+        values[i,:] .= throughputs(precision=precision, inmemory=true)
     end
 
     df.compression_disk = values[:, 1]
@@ -130,26 +130,78 @@ function tolTest()
     return df
 end
 
-function plot(df, title)
+function tolTest()
+    tols = [ 10.0^(-i) for i=1:10 ]
+    df = DataFrame()
+    df.tol = tols
+
+    values = zeros(length(df.tol),2)
+    for (i,tol) in enumerate(tols)
+        values[i,:] .= throughputs(tol=tol, inmemory=true)
+    end
+
+    df.compression_inmemory = values[:, 1]
+    df.decompression_inmemory = values[:, 2]
+
+    for (i,tol) in enumerate(tols)
+        values[i,:] .= throughputs(tol=tol, inmemory=false)
+    end
+
+    df.compression_disk = values[:, 1]
+    df.decompression_disk = values[:, 2]
+
+    return df
+end
+
+function plot(df_rate, df_tol, df_precision)
     fig = Figure()
-    ax = Axis(fig[1,1],
-              xlabel="rate (bps)",
-              ylabel="throughput (MiB/s)",
-              title=title,
-              xautolimitmargin=(0,0),
-              xticks = (1:4:64)
+    ax1 = Axis(fig[1,1],
+               xlabel="rate (bps)",
+               ylabel="throughput (MiB/s)",
+               title="Throughput x rate",
+               xautolimitmargin=(0,0),
+               xticks = (1:4:64)
              )
-    ln1 = lines!(ax, df.rate, df.compression_inmemory)
-    ln2 = lines!(ax, df.rate, df.decompression_inmemory)
-    ln3 = lines!(ax, df.rate, df.compression_disk)
-    ln4 = lines!(ax, df.rate, df.decompression_disk)
+    ax2 = Axis(fig[2,1],
+               xlabel="tolerance",
+               ylabel="throughput (MiB/s)",
+               title="Throughput x tolerance",
+               xscale=log10,
+               xautolimitmargin=(0,0),
+             )
+    ax3 = Axis(fig[3,1],
+               xlabel="precision (bit planes)",
+               ylabel="throughput (MiB/s)",
+               title="Throughput x precision",
+               xautolimitmargin=(0,0),
+               xticks = (1:4:64)
+             )
+
+    colors = [:blue, :red, :green, :orange]
+    ln1 = lines!(ax1, df_rate.rate, df_rate.compression_inmemory, color=colors[1])
+    ln2 = lines!(ax1, df_rate.rate, df_rate.decompression_inmemory, color=colors[2])
+    ln3 = lines!(ax1, df_rate.rate, df_rate.compression_disk, color=colors[3])
+    ln4 = lines!(ax1, df_rate.rate, df_rate.decompression_disk, color=colors[4])
+
+    lines!(ax2, df_tol.tol, df_tol.compression_inmemory, color=colors[1])
+    lines!(ax2, df_tol.tol, df_tol.decompression_inmemory, color=colors[2])
+    lines!(ax2, df_tol.tol, df_tol.compression_disk, color=colors[3])
+    lines!(ax2, df_tol.tol, df_tol.decompression_disk, color=colors[4])
+
+    lines!(ax3, df_precision.precision, df_precision.compression_inmemory, color=colors[1])
+    lines!(ax3, df_precision.precision, df_precision.decompression_inmemory, color=colors[2])
+    lines!(ax3, df_precision.precision, df_precision.compression_disk, color=colors[3])
+    lines!(ax3, df_precision.precision, df_precision.decompression_disk, color=colors[4])
+
     labels = ["Compression throughput", "Decompression throughput"]
     legend = Legend(fig, [[ln1, ln2], [ln3, ln4]], [labels, labels], ["In-memory", "Disk"], framevisible=false)
-    fig[1,2] = legend
+    fig[1:3,2] = legend
     display(fig)
     CairoMakie.save("../figs/throughput.pdf", fig)
     return nothing
 end
 
+precision_df = precisionTest()
+tol_df = tolTest()
 rate_df = rateTest()
-plot(rate_df, "Throughput x rate")
+plot(rate_df, tol_df, precision_df)
